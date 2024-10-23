c087898c31275cdc172d03ad6dafc6d1c0372a72
Lydia <lxma021@gmail.com>
2024-10-23 02:00:21 -0500

    Added a settings button

--- DP4/HomeView.swift
+++ DP4/HomeView.swift
@@ -8,23 +8,46 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack(spacing: 30) {  // Increased spacing between items
-            Text("Today’s Activity")
-                .font(.system(size: 28, weight: .bold, design: .default))  // Custom font size and weight
-                .padding(.top, 40)
+        NavigationView {
+            VStack(spacing: 30) {  // Increased spacing between items
+                Text("Today’s Activity")
+                // Custom font size and weight
+                    .font(.system(size: 28, weight: .bold, design: .default))
+                    .padding(.top, 40)
+                
+                // Replace this placeholder with your pie chart
+                Circle()
+                    .fill(Color.green)
+                    .frame(width: 200, height: 200)
+                    .padding()
+                
+                Text("Points Rule: 10 points every hour on Educational Activity")
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+            }
             
-            // Replace this placeholder with your pie chart
-            Circle()
-                .fill(Color.green)
-                .frame(width: 200, height: 200)
-                .padding()
+            .navigationTitle("Settings")
             
-            Text("Points Rule: 10 points every hour on Educational Activity")
-                .font(.subheadline)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 20)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        // Action for menu button
+                        print("Menu button tapped")
+                    }) {
+                        // Icon for menu button
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                            .font(.system(size: 30))
+                    }
+                }
+            }
         }
-
     }
 }
 
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
